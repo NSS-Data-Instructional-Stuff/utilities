eb2ae1bcdb253558714d6f45a0ff81892b9f9cea
Mahesh Rao <raomahes@gmail.com>
2021-04-09 08:55:33 -0500

    update class_website.py, lots of changes to make easier to use and more flexible

--- class_website.py
+++ class_website.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import json
 from collections import Counter
+import string
 
 
 def prepare_df(csv_path):
@@ -36,18 +37,26 @@ def check_file(sub_folder, name, path_to_repo):
         return 'nan'
 
 
-## Haven't tested yet ##
 def make_mirrored_images(existing_img_list, image_dir):
     existing_img_ppl = Counter([im[:-5] for im in existing_img_list if '.jpg' in im])
     need_flipped_names = [k+'1.jpg' for k, v in existing_img_ppl.items() if v == 1]
     pro_imgs = [im for im in existing_img_list if im in need_flipped_names]
     for im in pro_imgs:
         i = Image.open(image_dir+'/'+im)
-        i_mirror = ImageOps.mirror(i)
+        i_mirror = i.transpose(Image.FLIP_LEFT_RIGHT)
         i_mirror.save(image_dir + '/' + im[:-5] + '2.jpg')
 
 
-def make_cohort_dict(class_info, path_to_repo):
+def remove_punct(s):
+    return s.translate(str.maketrans('', '', string.punctuation))
+    
+    
+def combine_names(names, sep = '_'):
+    names = [remove_punct(n) for n in names]
+    return sep.join(names)
+
+
+def make_cohort_dict(class_info, path_to_repo, names_to_use):
     '''
     function to make cohort.json.
     arg class_info: pandas DataFrame, df including minimally the columns:
@@ -63,22 +72,24 @@ def make_cohort_dict(class_info, path_to_repo):
                         "github": fix_url(str(row['Github'])),
                         "linkedIn": fix_url(str(row['LinkedIn'])),
                         "portfolio": fix_url(str(row['Capstone (link)'])),
-                        "proImg": check_file("img", "{}1.jpg".format(row['First'].lower()), path_to_repo),
-                        "funImg": check_file("img", "{}2.jpg".format(row['First'].lower()), path_to_repo),
+                        "proImg": check_file("img", "{}1.jpg".format(combine_names([row[name].lower() for name in names_to_use])), path_to_repo),
+                        "funImg": check_file("img", "{}2.jpg".format(combine_names([row[name].lower() for name in names_to_use])), path_to_repo),
                         "video": fix_url(str(row['Capstone (video)'])),
-                        "resume": check_file("resume", "{}.pdf".format(row['First'].lower()), path_to_repo),
+                        "resume": check_file("resume", "{}.pdf".format(combine_names([row[name].lower() for name in names_to_use])), path_to_repo),
                         "email": str(row['Email'])}
         student_dict = {k:v for k, v in student_dict.items() if v != 'nan'}
         cohort_json['cohort'].append(student_dict)
     return cohort_json
 
 
-def make_cohort_json(csv_path):
+def make_cohort_json(csv_path, names_to_use = ['First']):
+    if type(names_to_use) != list:
+        names_to_use = [names_to_use]
     split_path = csv_path.split("/")
     repo_ind = [i for i, p in enumerate(split_path) if ".github.io" in p][0]
     path_to_repo = "/".join(split_path[:repo_ind+1])
     df = prepare_df(csv_path)
-    cohort_json = make_cohort_dict(df, path_to_repo)
+    cohort_json = make_cohort_dict(df, path_to_repo, names_to_use)
     outpath = "/".join(csv_path.split("/")[:-1])+"/cohort.json"
     with open(outpath, "w") as outfile:
         json.dump(cohort_json, outfile)
@@ -104,7 +115,6 @@ def convert_to_jpg(img_path):
     Converts image to JPEG and saves new file with .jpg extension
     '''
     img = Image.open(img_path)
-    # print(img.info)
     if img.format != "JPEG" or img_path[-4:] != ".jpg":
         img_path = '.'.join(img_path.split(".")[:-1]) + ".jpg" # join with '.' in case multiple . in path
         try:
@@ -119,9 +129,6 @@ def decrease_image_res(img_path):
     Decreases image resolution to 72dpi
     '''
     im = Image.open(img_path)
-    # print(img_path)
-    # print(im.info)
-    # print(im.info)
     try:
         if im.info['dpi'][0] > 72:
             im.save(img_path, dpi = (72, 72))
@@ -133,17 +140,33 @@ def find_image_height(img_path, aspect_ratio):
     print new image height with correct aspect ratio
     '''
     im = Image.open(img_path)
+    im_name = img_path.split('/')[-1]
+    new_height = round(im.width/aspect_ratio)
+    if new_height > im.height:
+        new_width = round(im.height * aspect_ratio)
+        print('new width for {}: '.format(im_name), new_width)
+    else:
+        print('new height for {}: '.format(im_name), new_height)
     
-    print(img_path.split('/')[-1])
-    print('new height: ', round(im.width/aspect_ratio))
-
+def find_max_aspect_ratio(img_dir, img_list):
+    pro_imgs = [im for im in img_list if '1' in im]
+    max_ar = 0
+    for img in pro_imgs:
+        im = Image.open(img_dir+'/'+img)
+        new_ar = im.width/im.height
+        if new_ar < 1:
+            max_ar = max(max_ar, new_ar)
+    print('max_ar: ', max_ar)
+    return max_ar
 
 def prepare_images(img_dir, ignore_files = ['.DS_Store']):
     existing_img_list = [im for im in os.listdir(img_dir) if im not in ignore_files]
+    max_ar = find_max_aspect_ratio(img_dir, existing_img_list)
     for img_name in existing_img_list:
         decrease_image_res(img_dir+'/'+img_name)
-    for img_name in existing_img_list:
         convert_to_jpg(img_dir+'/'+img_name)
-        find_image_height(img_dir+'/'+img_name, 0.82)
+        if '1' in img_name:
+            find_image_height(img_dir+'/'+img_name, max_ar)
+    existing_img_list = [im for im in os.listdir(img_dir) if im not in ignore_files]
     make_mirrored_images(existing_img_list, img_dir)
     print("prepared all images in {}".format(img_dir))
